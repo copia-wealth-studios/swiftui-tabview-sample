f123f7842c4a88c2b243829c49186217554b4c19
Cody Rayment <crayment16@gmail.com>
2025-02-18 15:10:29 -0700

    Initial Commit

---
+++ TabViewSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabViewSample
+//
+//  Created by Cody Rayment on 2025-02-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabViewSample/TabViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewSampleApp.swift
+//  TabViewSample
+//
+//  Created by Cody Rayment on 2025-02-18.
+//
+
+import SwiftUI
+
+@main
+struct TabViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
